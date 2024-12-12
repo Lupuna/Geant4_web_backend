0b2edb54a61ac87bfbf4c071f49a6d43f519b470
Lupuna <terragod427@gmail.com>
2024-12-12 16:26:32 +0700

    complete models without test coverage

---
+++ core/geant_tests_storage/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GeantTestsStorageConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'geant_tests_storage'
---
+++ core/geant_tests_storage/models.py
@@ -0,0 +1,51 @@
+from uuid import uuid4
+
+from django.utils.translation import gettext_lazy as _
+from django.db import models
+from datetime import time
+
+
+class Version(models.Model):
+    title = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = _("Version")
+        verbose_name_plural = _("Versions")
+
+    def __str__(self):
+        return self.title
+
+
+class TestResult(models.Model):
+    version = models.ForeignKey(Version, on_delete=models.SET_NULL, related_name='tests_results', null=True)
+    title = models.CharField(max_length=255)
+    date_to_update = models.DateTimeField(auto_now=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    day_to_delete = models.DateField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = _("TestResult")
+        verbose_name_plural = _("TestsResults")
+
+    def __str__(self):
+        return self.title
+
+    def clean(self):
+        super().clean()
+        if self.day_to_delete:
+            if self.creation_date.date() > self.day_to_delete:
+                raise ValueError(_('The "day_to_delete" field must be later than "creation_date".'))
+            if self.date_to_update.date() > self.day_to_delete:
+                raise ValueError(_('The "day_to_delete" field must be later than "date_to_update".'))
+
+
+class TestResultFile(models.Model):
+    uuid = models.UUIDField(default=uuid4, unique=True, editable=False)
+    test_result = models.ForeignKey(TestResult, on_delete=models.CASCADE, related_name='files')
+
+    class Meta:
+        verbose_name = _("TestResultFile")
+        verbose_name_plural = _("TestsResultsFiles")
+
+    def __str__(self):
+        return str(self.uuid)
