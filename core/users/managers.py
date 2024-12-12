8036fd23879241425308b340827d3746cfebb591
Lupuna <terragod427@gmail.com>
2024-12-12 12:45:19 +0700

    update UserManager

--- core/users/managers.py
+++ core/users/managers.py
@@ -6,11 +6,20 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_employee", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_employee_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_employee", True)
+        if extra_fields.get("is_employee") is not True:
+            raise ValueError("Employee must have is_employee=True.")
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_employee", True)
         if extra_fields.get("is_staff") is not True:
             raise ValueError("Superuser must have is_staff=True.")
 
