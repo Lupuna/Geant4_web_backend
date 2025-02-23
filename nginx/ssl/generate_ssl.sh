0b545988712bcd93f409eb4d93cd7aa2cb1e868f
TheAppleKingy <pridanov.ali.job@gmail.com>
2025-02-23 18:23:04 +0700

    correct ssl generate

--- nginx/ssl/generate_ssl.sh
+++ nginx/ssl/generate_ssl.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-mkdir -p /etc/nginx/ssl
+SSL_DIR="./nginx/ssl"
+
+mkdir -p "$SSL_DIR"
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
--keyout /etc/nginx/ssl/selfsigned.key \
--out /etc/nginx/ssl/selfsigned.crt \
--subj "/C=US/ST=State/L=City/O=Organization/OU=IT/CN=92.63.76.159"
+  -keyout "$SSL_DIR/selfsigned.key" \
+  -out "$SSL_DIR/selfsigned.crt" \
+  -subj "/C=US/ST=State/L=City/O=Organization/OU=IT/CN=92.63.76.159"
 
-echo "SSL made succsessfully."
+echo "SSL generated successfully in $SSL_DIR."
+
