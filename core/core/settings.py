1a673f94e61cf213656f86628423e869f4924f05
TheAppleKingy <pridanov.ali.job@gmail.com>
2025-02-26 20:28:25 +0700

    delete all static

--- core/core/settings.py
+++ core/core/settings.py
@@ -142,7 +142,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
