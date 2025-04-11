44e1a55a68674ad1458049f898afe7af902fcc94
TheAppleKingy <pridanov.ali.job@gmail.com>
2025-04-12 02:00:39 +0700

    correct paths

--- core/users/managers.py
+++ core/users/managers.py
@@ -20,6 +20,7 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_employee", True)
+        extra_fields.update({'is_active': True})
         if extra_fields.get("is_staff") is not True:
             raise ValueError("Superuser must have is_staff=True.")
 
