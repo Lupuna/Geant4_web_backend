2c592832f2038caf12210c30fc9419fd92ce4cca
Lupuna <terragod427@gmail.com>
2025-07-11 17:04:15 +0700

    complete problem with consistency data between db and elastic db

--- core/users/signals.py
+++ core/users/signals.py
@@ -8,6 +8,6 @@ from users.documents import UserExampleCommandDocument
 @receiver(post_save, sender=UserExampleCommand)
 def update_document(sender, instance, **kwargs):
     from loguru import logger
-    logger.info(f"SIGNAL: Обновление индекса для UserExampleCommand id={instance.pk} началось")
+    logger.info(f"SIGNAL: Обновление индекса для UserExampleCommand id={instance.pk} началось для user={instance.user}")
     UserExampleCommandDocument().update(instance)
-    logger.info(f"SIGNAL: Обновление индекса для UserExampleCommand id={instance.pk} закончилось")
+    logger.info(f"SIGNAL: Обновление индекса для UserExampleCommand id={instance.pk} закончилось для user={instance.user}")
